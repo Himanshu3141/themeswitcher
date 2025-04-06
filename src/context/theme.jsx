6d68f83a034842bee430bdaca3c4708a72682bc7
HIMANSHU. <himanshu3141@gmail.com>
2025-04-07 00:55:13 +0530

    refactor theme context implementation and update App component

---
+++ src/context/theme.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeContext.Provider value={{ themeMode, lightTheme, darkTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// export const useTheme = () => useContext(ThemeContext);
